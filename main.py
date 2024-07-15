643ab7d1338185953e44e8af26a3b6f39f6fc525
Paul Bailey <paul.ryan.bailey@gmail.com>
2024-07-15 18:35:56 -0500

    Fixed a few things

--- main.py
+++ main.py
@@ -1,31 +1,32 @@
 from can.interfaces import slcan
 from can import Message
 import can
-
+import time
 import threading
 
 
 class CANInterface:
     def __init__(
         self,
-        serial_interface_name: str = "/dev/ttyUSB0",
+        serial_interface_name: str = "/dev/ttyS0",
         can_interface_name="can0",
         bitrate=500000,
     ):
         self.interface_name: str = serial_interface_name
         self.bitrate: int = bitrate
         self.jetBus: slcan.slcanBus = slcan.slcanBus(
-            channel=serial_interface_name, bitrate=bitrate
+            channel=serial_interface_name
         )
 
         self.mainBus: can.interface.Bus = can.interface.Bus(
-            interface="socketcand", channel=can_interface_name
+            interface="socketcan", channel=can_interface_name, bitrate=bitrate
         )
 
     def receive_can_message(self):
         while True:
             try:
                 msg: Message = self.mainBus.recv()
+                # print(f'Sending {msg}')
                 self.jetBus.send(msg)
             except Exception as e:
                 print(f"Jetson to Bus error: {e}")
@@ -34,7 +35,9 @@ class CANInterface:
     def receive_serial_message(self):
         while True:
             try:
+                print('Trying')
                 msg: Message = self.jetBus.recv()
+                print(f'Recieved {msg}')
                 self.mainBus.send(msg)
             except Exception as e:
                 print(f"Bus to Jetson error: {e}")
@@ -43,19 +46,25 @@ class CANInterface:
 
 if __name__ == "__main__":
     can_interface: CANInterface = CANInterface(
-        serial_interface_name="/dev/ttyUSB0", can_interface_name="can0", bitrate=500000
+        serial_interface_name="/dev/ttyS0@115200", can_interface_name="can0", bitrate=500000
     )
 
     jetson_to_bus_thrd: threading.Thread = threading.Thread(
         target=can_interface.receive_serial_message
     )
+    jetson_to_bus_thrd.daemon = True
+    jetson_to_bus_thrd.name = "JetsonToBusThread_CAN"
     jetson_to_bus_thrd.start()
-    jetson_to_bus_thrd.setDaemon(True)
-    jetson_to_bus_thrd.setName("JetsonToBusThread_CAN")
+    print('Started jetson to bus thread')
 
     bus_to_jetson_thrd: threading.Thread = threading.Thread(
         target=can_interface.receive_can_message
     )
+    bus_to_jetson_thrd.daemon = True
+    bus_to_jetson_thrd.name = "BusToJetsonThread_CAN"
     bus_to_jetson_thrd.start()
-    bus_to_jetson_thrd.setDaemon(True)
-    bus_to_jetson_thrd.setName("BusToJetsonThread_CAN")
+    print('Started bus to jetson thread')
+
+    while True:
+        print("Running slcan to can adapter")
+        time.sleep(1)
